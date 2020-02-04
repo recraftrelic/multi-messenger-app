a583915601719ba5e53c1afdb0452de2a9326399
Yogesh Kapoor <yashkapoor8710@gmail.com>
2020-02-04 19:09:52 +0530

    PR-conflicts

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -21,32 +21,44 @@ const MainBrowserWindow: React.FunctionComponent<Props> = (props: Props) => {
   const [url, updateUrl] = useState<string>('');
   const [title, updateTitle] = useState<string>('');
   const [modal, setModal] = useState<boolean>(false);
-  const [value, setValue] = useState(localStorage.getItem('tabtitle'));
+  const [errorTitle, setErrorTitle] = useState<string>('');
+  const [errorUrl, setErrorUrl] = useState<string>('');
 
   useEffect(() => {
-    let getList = localStorage.getItem('tabtitle');
-    getList = JSON.parse(getList);
-    if(getList){
-      title: getList
-    }
-  });
+    let getList = JSON.parse(localStorage.getItem('tabtitle') || '[]');
+    let update = getList.map(item => {
+      const newTab: BrowserTab = {
+        url: item.url,
+        title: item.title,
+        config: {
+          webPreferences: {
+            partition: `persist:${item.title}`
+          }
+        }
+      }
+      return newTab
+    })
 
-  const { BrowserWindow } = remote
-  const focusedWindow: ElectronBrowserWindow = BrowserWindow.getFocusedWindow();
+    updateTabs(update);
+    
+  }, []);
 
   const handleNewTab = (): void => {
+
     if (!url || !title) {
       return
     }
     var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
     if(!regex .test(url)) {
-      return alert('Invalid URL...')
+      setErrorUrl('Invalid URL');
+      return
     }
     let matched = tabs.map(item => item.title).includes(title)
     if(matched){
-      return alert('Unique Title...')
+      setErrorTitle('Title Already Exists');
+      return 
     }
-
+    
     const newTab: BrowserTab = {
       url,
       title: title,
@@ -56,26 +68,36 @@ const MainBrowserWindow: React.FunctionComponent<Props> = (props: Props) => {
         }
       }
     }
-
     const modifiedTabs: BrowserTab[] = tabs.concat(newTab);
     updateTabs(modifiedTabs);
     setTabIndex(modifiedTabs.length - 1);
     updateUrl('');
     updateTitle('');
     setModal(false);
-    localStorage.setItem('tabtitle', JSON.stringify(title))
+    setErrorTitle('')
+    setErrorUrl('')
+    localStorage.setItem('tabtitle', JSON.stringify(modifiedTabs))
   }
 
+  const { BrowserWindow } = remote
+  const focusedWindow: ElectronBrowserWindow = BrowserWindow.getFocusedWindow();
   if(modal){
     focusedWindow.setBrowserView(modal);
   }
 
-  const onTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => updateTitle(event.target.value)
-  const onUrlChange = (event: React.ChangeEvent<HTMLInputElement>) => updateUrl(event.target.value)
+  const onTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    updateTitle(event.target.value)
+    setErrorTitle('')
+  }
+  const onUrlChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    updateUrl(event.target.value)
+    setErrorUrl('')
+  }
 
   const onRemovingTab = (index: number): void => {
     const tabItems: BrowserTab = tabs.filter((_,tabIndex: number) => tabIndex !== index)
     updateTabs(tabItems);
+    localStorage.setItem('tabtitle', JSON.stringify(tabItems))
   }
   return (
     <>
@@ -107,13 +129,15 @@ const MainBrowserWindow: React.FunctionComponent<Props> = (props: Props) => {
           )
         )
       }
-      <button className="urlview">{value}</button>
+      
       <Button className="new-tab" onClick={() => setModal(true)}>+</Button>
       <ModalBox className="mymodal" save="Save" cancel="Cancel" onClickCancel={() => setModal(false)} onClickSave={handleNewTab} modal={modal}>
         <label className="search-label">Title </label>
         <input className="search-tab" value={title} onChange={onTitleChange} />
+        {errorTitle ? <span className="errormsg">{errorTitle}</span> : null}
         <label className="search-label">Url </label>
         <input className="search-tab" value={url} onChange={onUrlChange} />
+        {errorUrl ? <span className="errormsg">{errorUrl}</span> : null}
       </ModalBox>
     </>
   )
